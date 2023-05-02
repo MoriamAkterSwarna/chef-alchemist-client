d6906cdbebe0bbf43d0f68fabfee24a135b60813
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2023-05-02 18:40:58 +0600

    single recipe details updated

--- src/Pages/Home/Main/SingleRecipeDetail.jsx
+++ src/Pages/Home/Main/SingleRecipeDetail.jsx
@@ -5,15 +5,16 @@ const SingleRecipeDetail = ({recipeDetail}) => {
     const {_id,likes,name,num_recipes,years_of_experience,picture,category__id,description
     } =recipeDetail;
     return (
-        <div className='grid sm:grid-cols-1 lg:grid-cols-2 ml-10'>
+        <div className='grid sm:grid-cols-1 lg:grid-cols-2 mb-10 lg:pl-10 py-10 bg-purple-100'>
             <div>
-                <img src={picture} alt="" />
+                <img src={picture} className='rounded-full' alt="" />
             </div>
             <div className='sm:mt-2 lg:mt-20 lg:ml-10'>
-                <h3 className='text-2xl'>Chef Name: {name}</h3>
-                <p className='mt-3'>{description}</p>
+                <h3 className='text-2xl font-bold text-fuchsia-700'>Chef Name: {name}</h3>
+                <p className='mt-3'><span className='font-bold text-xl'>Short Description: </span>{description}</p>
                 <p className='mt-6'><span className='font-bold text-xl'>Likes:</span> {likes}</p>
                 <p><span className='font-bold text-xl'>Number of Recipes:</span> {num_recipes}</p>
+                <p><span className='font-bold text-xl'>Experience:</span> {years_of_experience}</p>
 
             </div>
 
